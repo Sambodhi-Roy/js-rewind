19535a8ebc67dfbd5c029a7d1109e580d4ff4020
Sambodhi Roy <139252043+Sambodhi-Roy@users.noreply.github.com>
2025-01-19 21:34:46 +0000

    Functions added

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+function sayMyName(){
+    console.log("S");
+    console.log("A");
+    console.log("M");
+}
+
+// sayMyName - Reference of the function
+// sayMyName() - Executes the function
+ 
+sayMyName()
+
+function addTwoNumbers(number1,number2)
+{
+    console.log(number1 + number2);
+}
