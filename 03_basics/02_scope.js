0ffae752c5b2724080a2206de2bfe3335ad73aef
Sambodhi Roy <139252043+Sambodhi-Roy@users.noreply.github.com>
2025-01-21 19:14:55 +0000

    Scopes added

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -19,3 +19,46 @@ console.log(c);
 // This is because var doesnt follow the block scoping. The variables declared with var can be called outside the block too.
 
 // The scope in the console and the scope in the code(node) environment is different
+
+function one(){
+    const username = "sam"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    // console.log(website); -> Error (Out of scope)
+    
+    two()
+}
+
+one()
+
+if(true)
+{
+    const username = "Sam"
+    if(username === "Sam")
+    {
+        const website = "youtube"
+        console.log(username + website);
+    }
+
+    // console.log(website);    -> Out of scope
+}
+
+// console.log(username);   -> Out of scope
+
+addone(5)   // -> Doesnt throw an error when declared with the method below
+
+function addone(num)
+{
+    return num+1
+}
+
+
+const addTwo = function(num){
+    return num+2
+}
+
+addTwo(5)   // Throws an error if we try to access the function before its definition using Method 2
